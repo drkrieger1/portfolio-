989e4f381e1805501904e69e08ebfd5b07af77ba
Erik Plyushko <eplyushko@gmail.com>
2017-08-29 13:06:16 -0700

    server bug fixed

--- server.js
+++ server.js
@@ -9,3 +9,4 @@ app.use(express.static('./public'));
 app.listen(PORT, function(){
   console.log(`Portfolio is listening on port ${PORT}`);
 });
+app.get('/*', (request, response) => response.sendFile('index.html', {root: './public'}));
